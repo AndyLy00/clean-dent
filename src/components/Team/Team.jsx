54b681a84dc8279009b23f2a73a1eb2d7a5edd8c
AndyLy00 <andreia.cristina.siretanu@gmail.com>
2026-03-07 22:27:13 +0200

    Fix Carousel

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,5 +1,5 @@
 import './Team.scss';
-import Slider from "react-slick";
+import Slider from 'react-slick';
 import team1 from '/src/assets/images/Team/team1.JPG';
 import team2 from '/src/assets/images/Team/team2.JPG';
 import team3 from '/src/assets/images/Team/team3.JPG';
@@ -7,11 +7,19 @@ import team4 from '/src/assets/images/Team/team4.JPG';
 import team5 from '/src/assets/images/Team/team5.JPG';
 import team6 from '/src/assets/images/Team/team6.JPG';
 
+const teamMembers = [
+    { image: team1, name: 'Team Member Name', job: 'Team Member Job' },
+    { image: team2, name: 'Team Member Name', job: 'Team Member Job' },
+    { image: team3, name: 'Team Member Name', job: 'Team Member Job' },
+    { image: team4, name: 'Team Member Name', job: 'Team Member Job' },
+    { image: team5, name: 'Team Member Name', job: 'Team Member Job' },
+    { image: team6, name: 'Team Member Name', job: 'Team Member Job' }
+];
 
 const Team = () => {
-    let settings = {
+    const settings = {
         dots: false,
-        infinite: true,
+        infinite: teamMembers.length > 4,
         speed: 500,
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -20,11 +28,12 @@ const Team = () => {
         initialSlide: 0,
         autoplay: true,
         autoplaySpeed: 3000,
+        adaptiveHeight: false,
         responsive: [
             {
-                breakpoint: 576,
+                breakpoint: 1200,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 3
                 }
             },
             {
@@ -34,14 +43,13 @@ const Team = () => {
                 }
             },
             {
-                breakpoint: 1200,
+                breakpoint: 576,
                 settings: {
-                    slidesToShow: 3
+                    slidesToShow: 1
                 }
             }
         ]
     };
-    const emptyImage = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
 
     return (
         <div className="team" id="team">
@@ -49,66 +57,30 @@ const Team = () => {
                 <div className="row">
                     <div className="col">
                         <h2 className="h1">Echipa noastră</h2>
-                        <h3 className="h5">Îngrijire dentară realizată de oameni care pun pacientul pe primul loc</h3>
+                        <h3 className="h5">
+                            Îngrijire dentară realizată de oameni care pun pacientul pe primul loc
+                        </h3>
                     </div>
                 </div>
+
                 <div className="row team__slider">
-                    <div className="col col-12">
+                    <div className="col-12">
                         <Slider {...settings}>
-                            <div className="slider-element">
-                                <div className="team-member">
-                                    <img className="team-member__image" alt="" src={team1}/>
-                                    <div className="team-member__data">
-                                        <div className="team-member__name">Team Member Name</div>
-                                        <div className="team-member__job">Team Member Job</div>
+                            {teamMembers.map((member, index) => (
+                                <div className="slider-element" key={index}>
+                                    <div className="team-member">
+                                        <img
+                                            className="team-member__image"
+                                            alt={member.name}
+                                            src={member.image}
+                                        />
+                                        <div className="team-member__data">
+                                            <div className="team-member__name">{member.name}</div>
+                                            <div className="team-member__job">{member.job}</div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="slider-element">
-                                <div className="team-member">
-                                    <img className="team-member__image" alt="" src={team2}/>
-                                    <div className="team-member__data">
-                                        <div className="team-member__name">Team Member Name</div>
-                                        <div className="team-member__job">Team Member Job</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="slider-element">
-                                <div className="team-member">
-                                    <img className="team-member__image" alt="" src={team3}/>
-                                    <div className="team-member__data">
-                                        <div className="team-member__name">Team Member Name</div>
-                                        <div className="team-member__job">Team Member Job</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="slider-element">
-                                <div className="team-member">
-                                    <img className="team-member__image" alt="" src={team4}/>
-                                    <div className="team-member__data">
-                                        <div className="team-member__name">Team Member Name</div>
-                                        <div className="team-member__job">Team Member Job</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="slider-element">
-                                <div className="team-member">
-                                    <img className="team-member__image" alt="" src={team5}/>
-                                    <div className="team-member__data">
-                                        <div className="team-member__name">Team Member Name</div>
-                                        <div className="team-member__job">Team Member Job</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="slider-element">
-                                <div className="team-member">
-                                    <img className="team-member__image" alt="" src={team6}/>
-                                    <div className="team-member__data">
-                                        <div className="team-member__name">Team Member Name</div>
-                                        <div className="team-member__job">Team Member Job</div>
-                                    </div>
-                                </div>
-                            </div>
+                            ))}
                         </Slider>
                     </div>
                 </div>
