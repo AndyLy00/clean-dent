f6187988ce47fe35bd74ddfdc01bceb3c7040cd5
AndyLy00 <andreia.cristina.siretanu@gmail.com>
2026-04-26 20:44:48 +0300

    Third Slick Change

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -19,31 +19,33 @@ const teamMembers = [
 const Team = () => {
     const settings = {
         dots: false,
-        infinite: teamMembers.length > 1,
+        infinite: teamMembers.length > 4,
         speed: 500,
-        slidesToShow: 1,
+        slidesToShow: 4,
         slidesToScroll: 1,
         arrows: false,
         autoplay: true,
         autoplaySpeed: 3000,
-        mobileFirst: true,
         responsive: [
             {
-                breakpoint: 576,
+                breakpoint: 1200,
                 settings: {
-                    slidesToShow: 2
+                    slidesToShow: 3,
+                    slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 3
+                    slidesToShow: 2,
+                    slidesToScroll: 1
                 }
             },
             {
-                breakpoint: 1200,
+                breakpoint: 576,
                 settings: {
-                    slidesToShow: 4
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             }
         ]
