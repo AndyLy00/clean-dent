de0d83ae72162fdde8225b2a38f73ddc77c95624
AndyLy00 <andreia.cristina.siretanu@gmail.com>
2026-04-26 20:19:01 +0300

    Fix Slider Responsive STyles

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -23,29 +23,29 @@ const Team = () => {
         speed: 500,
         slidesToShow: 4,
         slidesToScroll: 1,
-        accessibility: true,
         arrows: false,
-        initialSlide: 0,
         autoplay: true,
         autoplaySpeed: 3000,
-        adaptiveHeight: false,
         responsive: [
             {
                 breakpoint: 1200,
                 settings: {
-                    slidesToShow: 3
+                    slidesToShow: 3,
+                    slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 2
+                    slidesToShow: 2,
+                    slidesToScroll: 1
                 }
             },
             {
-                breakpoint: 576,
+                breakpoint: 768,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             }
         ]
